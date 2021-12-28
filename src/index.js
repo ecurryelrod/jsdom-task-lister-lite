61a21de7bb3fda8009647bc74ffc6c8d7c76376e
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-12-27 17:27:34 -0700

    commit tree 9aa0fce9ad38c4a38e0a1d0e20c98dd9ac731949

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form")
+  const newTaskDescription = document.getElementById("new-task-description")
+  const newTaskList = document.getElementById("tasks")
+
+  newTaskForm.addEventListener("submit", createNewTask)
+
+  function createNewTask(event) {
+    event.preventDefault()
+    const newTask = document.createElement("li")
+    newTask.innerText = newTaskDescription.value
+    newTaskList.appendChild(newTask)
+    event.target.reset()
+
+    const listButton = document.createElement("button")
+    listButton.textContent = 'X'
+    newTask.appendChild(listButton)
+    listButton.onclick = function(e) {
+      newTaskList.removeChild(newTask)
+    }
+  }
 });
